034b7f2cc5424eef7c4442fe6683b288e9446bad
narayanai <30489304+narayanai@users.noreply.github.com>
2018-09-08 15:12:10 +0530

    commit

--- src/main/java/example/Example.java
+++ src/main/java/example/Example.java
@@ -2,6 +2,7 @@ package example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +18,11 @@ public class Example extends SpringBootServletInitializer {
 
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(Example.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Example.class);
 	}
 
 }
